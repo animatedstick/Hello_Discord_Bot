95d0c5c687776f9a195c65537c127f971584e628
DiscordBotsDevelopers20 <39799352+DiscordBotsDevelopers20@users.noreply.github.com>
2018-06-02 17:01:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,6 +56,12 @@ async def on_member_join(member):
         server = member.server
         msg = "**:tada: Welcome to {} {}. You are the {} User!**".format(member.server.name, member.mention, len(server.members))
         await bot.send_message(channel1, msg)
+    
+    elif server.id == "446967025773051905":
+        chan = bot.get_channel("446972812712738816")
+        server = member.server
+        msg = "**:tada: Welcome to {} {}. You are the {} User!**".format(member.server.name, member.mention, len(server.members))
+        await bot.send_message(channel1, msg)
     else:
         return
 
